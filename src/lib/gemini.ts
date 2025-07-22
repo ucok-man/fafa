7594b9f8b5827a2469f98cc679bdb0496af5ee5d
Alvin Odhi N <ucokkocu411@gmail.com>
2025-07-23 01:03:19 +0700

    feat: change llm model

--- src/lib/gemini.ts
+++ src/lib/gemini.ts
@@ -1,7 +1,7 @@
 import { GoogleGenAI } from "@google/genai";
 
 export enum GeminiModel {
-  GEMINI_2_0_FLASH = "gemini-2.0-flash",
+  GEMINI_1_5_FLASH = "gemini-1.5-flash",
 }
 
 export const gemini = new GoogleGenAI({
